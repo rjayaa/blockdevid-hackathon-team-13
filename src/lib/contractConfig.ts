c8269aae35e8bfb2a9e3ac175aa244a5237b14c2
Jenson Carlton Santoso <jens@Jensons-MacBook-Air.local>
2025-11-08 00:25:31 +0700

    pls bisa

--- src/lib/contractConfig.ts
+++ src/lib/contractConfig.ts
@@ -4,16 +4,16 @@ import CarbonTokenABI from './abi/CarbonToken.json';
 import ProjectCertificateABI from './abi/ProjectCertificate.json';
 import RetiredProofABI from './abi/RetiredProof.json';
 
-// --- ALAMAT KONTRAK (FINAL DEPLOYMENT V2 - FIX) ---
+// --- ALAMAT KONTRAK (FINAL DEPLOYMENT V2.2) ---
 // Alamat-alamat ini sudah live di Lisk Sepolia
 
 // Kontrak Utama (Otak)
-export const CORE_CONTRACT_ADDRESS = "0x1aa078a3b417d214319bA2fA6e6306b943aE5489";
+export const CORE_CONTRACT_ADDRESS = "0x1Bebc50E2A03e295Ae16A6a877e1B022903fA4b8";
 
 // Kontrak Token
-export const TOKEN_CONTRACT_ADDRESS = "0xb4b8ccD3e378167e604a06b38A2b72e077C1a1b7";
-export const CERTIFICATE_CONTRACT_ADDRESS = "0xE3303A79D46225A661092e62Ad3F9ED927ffD93E";
-export const PROOF_CONTRACT_ADDRESS = "0x84BA879eC7b0C92F54Ef8C8E15abf59448b125f1";
+export const TOKEN_CONTRACT_ADDRESS = "0x755705D35921E008B0C1aBB43719Af16BEd7D2f1";
+export const CERTIFICATE_CONTRACT_ADDRESS = "0x91edf5f53DF486220118dce53803Ec45b655B4cC";
+export const PROOF_CONTRACT_ADDRESS = "0x2A5a229DeB7c9ba6E67de2225276ef53963372C1";
 
 // --- ABI (PANDUAN FUNGSI) ---
 // Kita ambil hanya bagian '.abi' dari file JSON
